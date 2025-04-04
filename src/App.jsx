369fa4511161f17dd60ebd6f3dc725808d003eae
Diego <dfmontesinos@gmail.com>
2025-04-04 23:47:58 +0200

    add change, have erros

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from "aws-amplify";
+import awsConfig from "./aws-exports";
+import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+
+Amplify.configure(awsConfig);
 
 function App() {
   return (
-    <>
-      <h1>Welcome to Amplify</h1>
-    </>
+    <AmplifyAuthenticator>
+      <>
+        <div className="App">
+          <h1>Welcome to Amplify</h1>
+        </div>
+        <AmplifySignOut />
+      </>
+    </AmplifyAuthenticator>
   );
 }
 
