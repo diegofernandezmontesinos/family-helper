71b3a9c4fa3c9d6907660d58261847388afb9af8
Diego <dfmontesinos@gmail.com>
2025-04-11 22:12:58 +0200

    add solution

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,48 +1,315 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const createTodo = /* GraphQL */ `
-  mutation CreateTodo(
-    $input: CreateTodoInput!
-    $condition: ModelTodoConditionInput
+export const createList = /* GraphQL */ `
+  mutation CreateList(
+    $input: CreateListInput!
+    $condition: ModelListConditionInput
   ) {
-    createTodo(input: $input, condition: $condition) {
+    createList(input: $input, condition: $condition) {
       id
-      name
+      title
       description
+      imageKey
+      slug
+      listItems {
+        items {
+          id
+          title
+          quantity
+          done
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
       createdAt
       updatedAt
-      __typename
+      owner
     }
   }
 `;
-export const updateTodo = /* GraphQL */ `
-  mutation UpdateTodo(
-    $input: UpdateTodoInput!
-    $condition: ModelTodoConditionInput
+export const updateList = /* GraphQL */ `
+  mutation UpdateList(
+    $input: UpdateListInput!
+    $condition: ModelListConditionInput
   ) {
-    updateTodo(input: $input, condition: $condition) {
+    updateList(input: $input, condition: $condition) {
       id
-      name
+      title
       description
+      imageKey
+      slug
+      listItems {
+        items {
+          id
+          title
+          quantity
+          done
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
       createdAt
       updatedAt
-      __typename
+      owner
     }
   }
 `;
-export const deleteTodo = /* GraphQL */ `
-  mutation DeleteTodo(
-    $input: DeleteTodoInput!
-    $condition: ModelTodoConditionInput
+export const deleteList = /* GraphQL */ `
+  mutation DeleteList(
+    $input: DeleteListInput!
+    $condition: ModelListConditionInput
   ) {
-    deleteTodo(input: $input, condition: $condition) {
+    deleteList(input: $input, condition: $condition) {
       id
-      name
+      title
       description
+      imageKey
+      slug
+      listItems {
+        items {
+          id
+          title
+          quantity
+          done
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
       createdAt
       updatedAt
-      __typename
+      owner
+    }
+  }
+`;
+export const createListItem = /* GraphQL */ `
+  mutation CreateListItem(
+    $input: CreateListItemInput!
+    $condition: ModelListItemConditionInput
+  ) {
+    createListItem(input: $input, condition: $condition) {
+      id
+      title
+      quantity
+      done
+      list {
+        id
+        title
+        description
+        imageKey
+        slug
+        listItems {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      actions {
+        items {
+          id
+          action
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const updateListItem = /* GraphQL */ `
+  mutation UpdateListItem(
+    $input: UpdateListItemInput!
+    $condition: ModelListItemConditionInput
+  ) {
+    updateListItem(input: $input, condition: $condition) {
+      id
+      title
+      quantity
+      done
+      list {
+        id
+        title
+        description
+        imageKey
+        slug
+        listItems {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      actions {
+        items {
+          id
+          action
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const deleteListItem = /* GraphQL */ `
+  mutation DeleteListItem(
+    $input: DeleteListItemInput!
+    $condition: ModelListItemConditionInput
+  ) {
+    deleteListItem(input: $input, condition: $condition) {
+      id
+      title
+      quantity
+      done
+      list {
+        id
+        title
+        description
+        imageKey
+        slug
+        listItems {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      actions {
+        items {
+          id
+          action
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const createAction = /* GraphQL */ `
+  mutation CreateAction(
+    $input: CreateActionInput!
+    $condition: ModelActionConditionInput
+  ) {
+    createAction(input: $input, condition: $condition) {
+      id
+      action
+      listItem {
+        id
+        title
+        quantity
+        done
+        list {
+          id
+          title
+          description
+          imageKey
+          slug
+          createdAt
+          updatedAt
+          owner
+        }
+        actions {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const updateAction = /* GraphQL */ `
+  mutation UpdateAction(
+    $input: UpdateActionInput!
+    $condition: ModelActionConditionInput
+  ) {
+    updateAction(input: $input, condition: $condition) {
+      id
+      action
+      listItem {
+        id
+        title
+        quantity
+        done
+        list {
+          id
+          title
+          description
+          imageKey
+          slug
+          createdAt
+          updatedAt
+          owner
+        }
+        actions {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const deleteAction = /* GraphQL */ `
+  mutation DeleteAction(
+    $input: DeleteActionInput!
+    $condition: ModelActionConditionInput
+  ) {
+    deleteAction(input: $input, condition: $condition) {
+      id
+      action
+      listItem {
+        id
+        title
+        quantity
+        done
+        list {
+          id
+          title
+          description
+          imageKey
+          slug
+          createdAt
+          updatedAt
+          owner
+        }
+        actions {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
     }
   }
 `;
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,35 +1,215 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const getTodo = /* GraphQL */ `
-  query GetTodo($id: ID!) {
-    getTodo(id: $id) {
+export const getList = /* GraphQL */ `
+  query GetList($id: ID!) {
+    getList(id: $id) {
       id
-      name
+      title
       description
+      imageKey
+      slug
+      listItems {
+        items {
+          id
+          title
+          quantity
+          done
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
       createdAt
       updatedAt
-      __typename
+      owner
     }
   }
 `;
-export const listTodos = /* GraphQL */ `
-  query ListTodos(
-    $filter: ModelTodoFilterInput
+export const listLists = /* GraphQL */ `
+  query ListLists(
+    $filter: ModelListFilterInput
     $limit: Int
     $nextToken: String
   ) {
-    listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    listLists(filter: $filter, limit: $limit, nextToken: $nextToken) {
       items {
         id
-        name
+        title
         description
+        imageKey
+        slug
+        listItems {
+          nextToken
+        }
         createdAt
         updatedAt
-        __typename
+        owner
       }
       nextToken
-      __typename
+    }
+  }
+`;
+export const getListItem = /* GraphQL */ `
+  query GetListItem($id: ID!) {
+    getListItem(id: $id) {
+      id
+      title
+      quantity
+      done
+      list {
+        id
+        title
+        description
+        imageKey
+        slug
+        listItems {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      actions {
+        items {
+          id
+          action
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const listListItems = /* GraphQL */ `
+  query ListListItems(
+    $filter: ModelListItemFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listListItems(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        title
+        quantity
+        done
+        list {
+          id
+          title
+          description
+          imageKey
+          slug
+          createdAt
+          updatedAt
+          owner
+        }
+        actions {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      nextToken
+    }
+  }
+`;
+export const getAction = /* GraphQL */ `
+  query GetAction($id: ID!) {
+    getAction(id: $id) {
+      id
+      action
+      listItem {
+        id
+        title
+        quantity
+        done
+        list {
+          id
+          title
+          description
+          imageKey
+          slug
+          createdAt
+          updatedAt
+          owner
+        }
+        actions {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const listActions = /* GraphQL */ `
+  query ListActions(
+    $filter: ModelActionFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listActions(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        action
+        listItem {
+          id
+          title
+          quantity
+          done
+          createdAt
+          updatedAt
+          owner
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      nextToken
+    }
+  }
+`;
+export const searchLists = /* GraphQL */ `
+  query SearchLists(
+    $filter: SearchableListFilterInput
+    $sort: SearchableListSortInput
+    $limit: Int
+    $nextToken: String
+    $from: Int
+  ) {
+    searchLists(
+      filter: $filter
+      sort: $sort
+      limit: $limit
+      nextToken: $nextToken
+      from: $from
+    ) {
+      items {
+        id
+        title
+        description
+        imageKey
+        slug
+        listItems {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      nextToken
+      total
     }
   }
 `;
